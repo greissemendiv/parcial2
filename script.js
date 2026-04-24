5917c0045cd69e29397fcf81cdfe1e4ac0d58749
Greisse Mendivelso <est.greisse.mendiv@unimilitar.edu.co>
2026-04-24 12:32:34 -0500

    nueva funcionalidad: implementación bresenham y trazado de líneas

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-function plotPixel(ctx, x, y, color = "#000") {
+function plotPixel(ctx, x, y, color = "#0FF") {
     ctx.fillStyle = color;
     ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+function bresenhamLine(x0, y0, x1, y1, color="#0FF") {
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+
+    let err = dx - dy;
+
+    while (true) {
+        plotPixel(ctx, x0, y0, color);
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
